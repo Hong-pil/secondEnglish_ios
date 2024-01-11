ee752570363a8a72a2fd945ce596ff8ddfcea459
kimhongpil <idpil@kimhongpilui-MacBookPro.local>
2024-01-11 14:39:10 +0900

    first commit

---
+++ SecondEnglish/SecondEnglishApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecondEnglishApp.swift
+//  SecondEnglish
+//
+//  Created by kimhongpil on 1/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SecondEnglishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
